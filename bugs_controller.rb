d3fb6218f84f49ece6419bf251d3f20bb404397f
NimraFatima61997 <38766672+NimraFatima61997@users.noreply.github.com>
2020-08-31 22:18:44 +0500

    Add files via upload

---
+++ bugs_controller.rb
@@ -0,0 +1,107 @@
+class BugsController < ApplicationController
+  before_action :set_bug, only: [:show, :edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token
+ 
+  def index 
+    @project = get_project
+    @bugs = @project.bugs
+    authorize @bugs
+  end
+
+  def show
+    @project = get_project
+  end
+
+  def new
+    @bug = Bug.new
+    @user=current_user
+    @project = get_project
+    authorize @bug 
+  end
+
+  def edit
+    @project = get_project
+  end
+
+  def create 
+   
+      @bug =  current_user.bug_reports.new(bug_params)
+      @bug.project=get_project
+      authorize @bug
+      if @bug.save
+        redirect_to projects_path,notice: " Bug was successfully added."    
+      else
+        redirect_to new_project_bug_path,notice: " Bug not created."
+    end
+  end
+
+  def update
+    authorize @bug
+    if @bug.update(bug_params)
+      redirect_to project_bug_path,notice: " Bug was successfully updated."
+    else
+      redirect_to edit_project_bug_path,notice: " Bug was not updated."
+    end
+  end
+
+  def destroy
+    authorize @bug
+    if @bug.destroy
+      redirect_to project_bugs_path, notice: 'Bug was successfully destroyed.' 
+    else
+      redirect_to project_bugs_path,notice: " Bug was not destoryed."
+    end
+  end
+ 
+  def bug_status_options
+    if(params[:bug_type]=="Feature")
+      status_array=["new", "started", "completed"]
+    else
+      status_array=["new", "started", "resolved"]
+    end
+    respond_to do |format|
+      format.json {
+      render json: {status: status_array}}
+    end
+  end
+
+  def change_status
+    @bug = Bug.find(params[:bug])
+    authorize @bug
+    raw_parameters = { :bugs => { :status => params[:status] } }
+    parameters = ActionController::Parameters.new(raw_parameters)
+    parameters.require(:bugs).permit!
+    if @bug.update_attributes( parameters.require(:bugs).permit!)
+      redirect_to projects_path,notice: " Bug Status was successfully updated."
+     end
+  end
+
+   def assign_resolver
+    @bug = Bug.find(params[:bug])
+    authorize @bug
+    if current_user.bugs << @bug
+      redirect_to projects_path,notice: " Bug resolver was successfully added."
+    end
+  end
+
+  def remove_resolver
+    @bug = Bug.find(params[:bug])
+    authorize @bug
+    if current_user.bugs.destroy(@bug)
+      redirect_to projects_path,notice: " Bug resolver was successfully removed."
+    end
+  end
+
+  private
+    def set_bug
+      @bug = Bug.find(params[:id])
+    end
+
+   def get_project
+    @project = Project.find(params[:project_id]) 
+   end
+   
+    def bug_params
+      params.require(:bug).permit(:title, :deadline, :screenshot, :bug_type, :status)
+    end
+end
---
+++ projects_controller.rb
@@ -0,0 +1,107 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @user=current_user
+    
+    if policy(current_user).manager?
+    @projects=current_user.projects
+    elsif policy(current_user).developer?
+      @projects = current_user.projects
+    elsif policy(current_user).qa?
+      @projects = Project.all
+    end 
+    if @projects.empty?
+    else
+      authorize @projects
+    end
+
+      
+  end
+
+  def adduser
+    @users = User.all
+    @project = Project.find(params[:id])
+  end
+
+  def add
+    @user = User.find(params[:user])
+    @project = Project.find(params[:id])
+  
+    authorize @project
+    @project.users<<@user
+
+    redirect_to '/projects/'+@project.id.to_s+'/adduser',notice: @user.user_type + " was successfully added."
+  end
+
+  def removeuser
+    @project = Project.find(params[:id])
+    @users=Array.new
+    @project.users.each do |user|
+      @users<<user   
+    end
+  end
+
+  def remove
+    @user = User.find(params[:user])
+    @project = Project.find(params[:id])
+    authorize @project
+    @project.users.delete(@user)
+    redirect_to '/projects/'+@project.id.to_s+'/removeuser',notice: @user.user_type + " was successfully removed."
+  end
+
+  def show
+  end
+
+  def new
+    @project = Project.new
+    authorize @project
+  end
+
+  def edit
+  end
+
+  def create
+    @project=current_user.projects.create(project_params)
+    authorize @project
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to @project, notice: 'Project was successfully created.' }
+        format.json { render :show, status: :created, location: @project }
+      else
+        format.html { render :new }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @project.update(project_params)
+        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
+        format.json { render :show, status: :ok, location: @project }
+      else
+        format.html { render :edit }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @project.destroy
+    respond_to do |format|
+      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_project
+      @project = Project.find(params[:id])
+      authorize @project
+    end
+
+    def project_params
+      params.require(:project).permit(:title)
+    end
+end
