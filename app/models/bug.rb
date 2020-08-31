f3521d6ff3dba90710bc23c93ac81cb56dfaf6f2
NimraFatima61997 <38766672+NimraFatima61997@users.noreply.github.com>
2020-08-31 22:26:11 +0500

    Update bug.rb
    add validation

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,4 +1,9 @@
 class Bug < ApplicationRecord
+  validates :title, presence: true,length: { maximum: 40 },uniqueness: true
+  validates :screenshot, presence: true
+  validates :deadline, presence: true
+  validates :status, presence: true
+  validates :bug_type, presence: true
   mount_uploader :screenshot, ScreenshotUploader
   belongs_to :user
   belongs_to :project
